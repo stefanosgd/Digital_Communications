9f0d7c142b7a076f70e5995c3bc21d21ed2128a9
Stefanos Gregory Demetriou <sgdemetriou@gmail.com>
2019-02-12 16:18:44 +0000

    Fixed bug

--- wrtq84.py
+++ wrtq84.py
@@ -16,14 +16,12 @@ def encoding(message, w, l):
     encoded_bit_message = ''
     p = message[0]
     for i in range(0, len(message)-1):
-
         if len(p) > len(dictionary):
             check = -1
         elif p[-1] == dictionary[len(p)-1]:
             check = 0
         else:
             check = dictionary.find(p)
-
         if (check != -1) and (len(p) < lookahead):
             dictionary = dictionary[check:]
             p = p+message[i+1]
@@ -68,12 +66,13 @@ def decoding(encoded):
 def convert_to_list(bit_message, w, l):
     converted_list = []
     i = 0
-    while i < len(bit_message) - (w+l):
+    while i < len(bit_message)-(w+l):
         converted_list.append((int(bit_message[i:w+i], 2), int(bit_message[w+i:w+l+i], 2), (bit_message[w+l+i])))
         i += w+l+1
-    if (i+w) > len(bit_message):
-        length = len(converted_list)
-        converted_list[length-1] = (converted_list[length-1][0], converted_list[length-1][1], '_')
+    if (i+w+l) == len(bit_message):
+        converted_list.append((int(bit_message[i:w+i], 2), int(bit_message[w+i:w+l+i], 2), '_'))
+    elif (i+w+l) > len(bit_message):
+        converted_list[-1] = (converted_list[-1][0], converted_list[-1][1], '_')
     return converted_list
 
 
@@ -88,7 +87,7 @@ def read_file(f_name):
 
 def write_file(f_name, f_extension, output):
     new_file = open(f_name+f_extension, 'wb')
-    new_file.write(output)
+    bt(output).tofile(new_file)
     new_file.close()
 
 
@@ -102,7 +101,7 @@ def compress(file_name, window, lookahead):
     print("Original:", size)
     print("Compressed:", len(bit_encode))
     print("Compression rate:", size / len(bit_encode))
-    write_file(file_name+"_out", '', bt(bit_encode).tobytes())
+    write_file(file_name+"_out", '', bit_encode)
 
 
 def decompress(file_name, window, lookahead, save_extension):
@@ -110,22 +109,29 @@ def decompress(file_name, window, lookahead, save_extension):
     input_again, new_input_size = read_file(file_name)
     input_list_convert = convert_to_list(input_again, window, lookahead)
     output_message_new = decoding(input_list_convert)
-    write_file(file_name+'_decompressed', save_extension, bt(output_message_new).tobytes())
+    write_file(file_name+'_decompressed', save_extension, output_message_new)
     stop = timeit.default_timer()
     print("Decompression took", stop - start, "seconds")
 
 
-compress('Tests/test1.txt', 16, 7)
-decompress('Tests/test1_out', 16, 7, '.txt')
-# compress('Tests/test2.txt', 14, 7)
-# decompress('Tests/test2_out', 14, 7, '.txt')
-# compress('Tests/test3.txt', 16, 7)
-# decompress('Tests/test3_out', 16, 7, '.txt')
-# compress('Tests/test4.txt', 17, 7)
-# decompress('Tests/test4_out', 17, 7, '.txt')
+print("Test 1")
+compress('Tests/test1.txt', 12, 7)
+decompress('Tests/test1_out', 12, 7, '.txt')
+print("Test 2")
+compress('Tests/test2.txt', 14, 7)
+decompress('Tests/test2_out', 14, 7, '.txt')
+print("Test 3")
+compress('Tests/test3.txt', 16, 7)
+decompress('Tests/test3_out', 16, 7, '.txt')
+print("Test 4")
+compress('Tests/test4.txt', 17, 7)
+decompress('Tests/test4_out', 17, 7, '.txt')
+print("Test 5")
 compress('Tests/test5.txt', 19, 7)
 decompress('Tests/test5_out', 19, 7, '.txt')
-# compress('Tests/test6.txt', 16, 8)
-# decompress('Tests/test6_out', 16, 8, '.txt')
+# print("Test 6")
+# compress('Tests/test6.txt', 16, 7)
+# decompress('Tests/test6_out', 16, 7, '.txt')
+# print("Test Image")
 # compress('Tests/test_image.jpg', 16, 5)
 # decompress('Tests/test_image_out', 16, 5, '.jpg')
